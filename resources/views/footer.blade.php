9674dc4638d55f0a4a4503245e960fa6174fe3db
dinandt <hubertsdinand@gmail.com>
2022-02-15 12:48:31 +0100

    initial commit

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,25 @@
+<footer class="p-4 bg-white rounded-lg shadow md:px-6 md:py-8 dark:bg-amber-500">
+    <div class="sm:flex sm:items-center sm:justify-between">
+        <a href="https://flowbite.com" target="_blank" class="flex items-center mb-4 sm:mb-0">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/9/98/International_Pok%C3%A9mon_logo.svg" class="mr-4 h-8" alt="Logo" />
+            <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">CodeMon</span>
+        </a>
+        <ul class="flex flex-wrap items-center mb-6 sm:mb-0">
+            <li>
+                <a href="#" class="mr-4 text-sm text-pink-500 md:dark:hover:text-zinc-200 hover:underline md:mr-6 dark:text-white">About</a>
+            </li>
+            <li>
+                <a href="#" class="mr-4 text-sm text-pink-500 md:dark:hover:text-zinc-200 hover:underline md:mr-6 dark:text-white">Privacy Policy</a>
+            </li>
+            <li>
+                <a href="#" class="mr-4 text-sm text-pink-500 md:dark:hover:text-zinc-200 hover:underline md:mr-6 dark:text-white">Licensing</a>
+            </li>
+            <li>
+                <a href="#" class="mr-4 text-sm text-pink-500 md:dark:hover:text-zinc-200 hover:underline md:mr-6 dark:text-white">Contact</a>
+            </li>
+        </ul>
+    </div>
+    <hr class="my-6 border-pink-200 sm:mx-auto dark:border-pink-700 lg:my-8" />
+    <span class="block text-sm text-pink-500 sm:text-center dark:text-white md:dark:hover:text-zinc-200">© 2022 <a onclick="window.location.replace('/')" target="_blank" class="hover:underline">LandstedeMon™</a>. All Rights Reserved.
+    </span>
+</footer>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+  <link href="{{ asset('css/sass.css') }}" rel="stylesheet">
+  <link href="{{ asset('js/app.js') }}" rel="text/javascript">
+  <script src="https://unpkg.com/flowbite@1.3.3/dist/flowbite.js"></script>
+</head>
+<body>
+
+<?php
+include('../resources/views/header.blade.php');
+?>
+
+
+
+<?php
+include('../resources/views/footer.blade.php');
+?>
+
+</html>
