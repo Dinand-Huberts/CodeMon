f0a5d66b359145dc2ba3deff9ee8825f71f10a78
dinandt <hubertsdinand@gmail.com>
2022-02-16 13:11:35 +0100

    changed footer hrfs

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="p-4 bg-white rounded-lg shadow md:px-6 md:py-8 dark:bg-amber-500 absolute bottom-0 w-full">
     <div class="sm:flex sm:items-center sm:justify-between">
-        <a href="./" class="flex items-center mb-4 sm:mb-0">
+        <a onclick="window.location.replace('./')" class="flex items-center mb-4 sm:mb-0">
             <img src="https://upload.wikimedia.org/wikipedia/commons/9/98/International_Pok%C3%A9mon_logo.svg" class="mr-4 h-8" alt="Logo" />
             <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">CodeMon</span>
         </a>
@@ -20,6 +20,6 @@
         </ul>
     </div>
     <hr class="my-6 border-pink-200 sm:mx-auto dark:border-pink-700 lg:my-8" />
-    <span class="block text-sm text-pink-500 sm:text-center dark:text-white md:dark:hover:text-zinc-200">© 2022 <a onclick="window.location.replace('/')" target="_blank" class="hover:underline">LandstedeMon™</a>. All Rights Reserved.
+    <span class="block text-sm text-pink-500 sm:text-center dark:text-white md:dark:hover:text-zinc-200">© 2022 <a onclick="window.location.replace('./')" target="_blank" class="hover:underline">LandstedeMon™</a>. All Rights Reserved.
     </span>
 </footer>   
