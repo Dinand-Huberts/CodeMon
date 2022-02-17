ebecb85d6bba8bbba0d9688a37608c21f6c63d72
dinandt <hubertsdinand@gmail.com>
2022-02-17 09:58:23 +0100

    made includes.php (for head tag)

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -2,13 +2,9 @@
 <html>
 
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-  <link href="{{ asset('css/sass.css') }}" rel="stylesheet">
-  <link href="{{ asset('css/main.css') }}" rel="stylesheet">
-  <link href="{{ asset('js/app.js') }}" rel="text/javascript">
-  <script src="https://unpkg.com/flowbite@1.3.3/dist/flowbite.js"></script>
+<?php
+include('../resources/views/includes.php');
+?>
 </head>
 
 <body>
---
+++ resources/views/includes.php
@@ -0,0 +1,10 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link href="{{ asset('/css/app.css') }}" rel="stylesheet">
+<link href="{{ asset('/css/sass.css') }}" rel="stylesheet">
+<link href="{{ asset('/css/main.css') }}" rel="stylesheet">
+<link href="{{ asset('js/app.js') }}" rel="text/javascript">
+<script src="https://unpkg.com/flowbite@1.3.3/dist/flowbite.js"></script>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@500&display=swap" rel="stylesheet">
