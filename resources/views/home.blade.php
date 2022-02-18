91cc13e6929f98f0cc13c620c77607d422e05a89
Dr. Doofus <>
2022-02-18 10:04:14 +0100

    parallax implemented

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,6 +11,19 @@ include('../resources/views/includes.php');
 include('../resources/views/header.blade.php');
 ?>
 
+<!-- Title + logo -->
+
+<div class="parallax">
+    <div id="group6" class="parallax__group">
+      <div class="parallax__layer parallax__layer--back relative w-full h-[400vh]" style="background-image: url('./img/home-bg.png'); background-size: cover; background-repeat: no-repeat; background-position: center;">
+      </div>
+      <div class="parallax__layer parallax__layer--base">
+        <div class="title">Base Layer</div>
+      </div>
+    </div>
+  </div>
+
+<!-- Page content -->
 
 
 
