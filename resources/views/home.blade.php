4fce988c6e6002abc658fdcdcbbaa869ca183cdd
dinandt <hubertsdinand@gmail.com>
2022-02-21 11:23:03 +0100

    home page template made

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,37 +8,67 @@
 </head>
 
 <body>
-<div class="parallax">
+  <div class="parallax">
     <?php
     include('../resources/views/header.blade.php');
     ?>
 
-    <!-- Title + logo -->
 
 
 
-
-    <div id="group3" class="parallax__group absolute">
+    <div id="group" class="parallax__group absolute">
       <div class="parallax__layer parallax__layer--fore">
-        <div class="title">Foreground Layer</div>
+        <div class="home_content backdrop-blur-sm rounded-2xl flex" style="background-color: rgba(125, 125, 125, 0.2)">
+          <div id="image_home" class="image_home h-[50%] w-[50%] p-6 ">
+            <img src="./img/kaart.png" alt="kaart">
+          </div>
+          <div id="text_home" class="w-[50%] ml-[3%] p-6">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed ligula porttitor, varius nisi vitae, varius urna. Morbi nulla eros, finibus ut felis vitae, lobortis mattis sapien. Nunc viverra euismod metus vitae mollis. Curabitur augue nisi, molestie in velit sed, ultrices dapibus nisi. Suspendisse potenti. Curabitur et dolor a mi vestibulum fermentum. Nunc eget quam accumsan, bibendum leo sed, lacinia odio. Vivamus eu ante accumsan, laoreet dolor et, lobortis eros. Ut ut neque rhoncus, sodales risus sit amet, placerat lectus. Mauris consectetur ligula ac dui facilisis, et rutrum magna blandit. Maecenas commodo, tellus sed pharetra dignissim, lorem nunc vulputate sapien, sed facilisis lectus quam vel arcu. Vestibulum congue nunc velit, efficitur molestie sem convallis in. Ut risus risus, mollis convallis felis ac, egestas facilisis nulla.
+          </div>
+        </div>
       </div>
       <div class="parallax__layer parallax__layer--base">
         <div class="h-[400vh] w-screen" style="background-image: url('./img/home-bg.png'); background-size: cover; background-repeat: no-repeat; background-position: center;"></div>
       </div>
     </div>
-    <div id="group3" class="parallax__group absolute">
+
+    <div id="group" class="parallax__group absolute">
       <div class="parallax__layer parallax__layer--fore">
-        <div class="title">Foreground Layer</div>
+        <div class="home_content backdrop-blur-sm rounded-2xl flex" style="background-color: rgba(125, 125, 125, 0.2)">
+          <div id="text_home" class="w-[50%] ml-[3%] p-6">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed ligula porttitor, varius nisi vitae, varius urna. Morbi nulla eros, finibus ut felis vitae, lobortis mattis sapien. Nunc viverra euismod metus vitae mollis. Curabitur augue nisi, molestie in velit sed, ultrices dapibus nisi. Suspendisse potenti. Curabitur et dolor a mi vestibulum fermentum. Nunc eget quam accumsan, bibendum leo sed, lacinia odio. Vivamus eu ante accumsan, laoreet dolor et, lobortis eros. Ut ut neque rhoncus, sodales risus sit amet, placerat lectus. Mauris consectetur ligula ac dui facilisis, et rutrum magna blandit. Maecenas commodo, tellus sed pharetra dignissim, lorem nunc vulputate sapien, sed facilisis lectus quam vel arcu. Vestibulum congue nunc velit, efficitur molestie sem convallis in. Ut risus risus, mollis convallis felis ac, egestas facilisis nulla.
+          </div>
+          <div id="image_home" class="h-[50%] w-[50%] p-6">
+            <img src="./img/kaart.png" alt="kaart">
+          </div>
+        </div>
       </div>
     </div>
-    <div id="group3" class="parallax__group absolute">
+
+    <div id="group" class="parallax__group absolute">
       <div class="parallax__layer parallax__layer--fore">
-        <div class="title">Foreground Layer</div>
+        <div class="home_content backdrop-blur-sm rounded-2xl flex" style="background-color: rgba(125, 125, 125, 0.2)">
+          <div id="image_home" class="image_home h-[50%] w-[50%] p-6 ">
+            <img src="./img/kaart.png" alt="kaart">
+          </div>
+          <div id="text_home" class="w-[50%] ml-[3%] p-6">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed ligula porttitor, varius nisi vitae, varius urna. Morbi nulla eros, finibus ut felis vitae, lobortis mattis sapien. Nunc viverra euismod metus vitae mollis. Curabitur augue nisi, molestie in velit sed, ultrices dapibus nisi. Suspendisse potenti. Curabitur et dolor a mi vestibulum fermentum. Nunc eget quam accumsan, bibendum leo sed, lacinia odio. Vivamus eu ante accumsan, laoreet dolor et, lobortis eros. Ut ut neque rhoncus, sodales risus sit amet, placerat lectus. Mauris consectetur ligula ac dui facilisis, et rutrum magna blandit. Maecenas commodo, tellus sed pharetra dignissim, lorem nunc vulputate sapien, sed facilisis lectus quam vel arcu. Vestibulum congue nunc velit, efficitur molestie sem convallis in. Ut risus risus, mollis convallis felis ac, egestas facilisis nulla.
+          </div>
+
+        </div>
       </div>
     </div>
-    <div id="group3" class="parallax__group absolute">
+
+    <div id="group" class="parallax__group absolute">
       <div class="parallax__layer parallax__layer--fore">
-        <div class="title">Foreground Layer</div>
+        <div class="home_content backdrop-blur-sm rounded-2xl flex" style="background-color: rgba(125, 125, 125, 0.2)">
+          <div id="text_home" class="w-[50%] ml-[3%] p-6">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed ligula porttitor, varius nisi vitae, varius urna. Morbi nulla eros, finibus ut felis vitae, lobortis mattis sapien. Nunc viverra euismod metus vitae mollis. Curabitur augue nisi, molestie in velit sed, ultrices dapibus nisi. Suspendisse potenti. Curabitur et dolor a mi vestibulum fermentum. Nunc eget quam accumsan, bibendum leo sed, lacinia odio. Vivamus eu ante accumsan, laoreet dolor et, lobortis eros. Ut ut neque rhoncus, sodales risus sit amet, placerat lectus. Mauris consectetur ligula ac dui facilisis, et rutrum magna blandit. Maecenas commodo, tellus sed pharetra dignissim, lorem nunc vulputate sapien, sed facilisis lectus quam vel arcu. Vestibulum congue nunc velit, efficitur molestie sem convallis in. Ut risus risus, mollis convallis felis ac, egestas facilisis nulla.
+          </div>
+          <div id="image_home" class="h-[50%] w-[50%] p-6">
+            <img src="./img/kaart.png" alt="kaart">
+          </div>
+        </div>
       </div>
     </div>
 
@@ -49,7 +79,7 @@
     <?php
     include('../resources/views/footer.blade.php');
     ?>
-    </div>
+  </div>
 </body>
 
 </html>
