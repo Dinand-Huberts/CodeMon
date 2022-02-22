b267afa550eced1707c54929c039efb0b3e63b31
jploeg <jploeg3450@student.landstede.nl>
2022-02-22 09:43:18 +0100

    link maar beter

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -39,7 +39,7 @@
 </div>
 
 
-<a href="{{ route('quiz') }}"  type="button" class="mt-40 ml-10 text-black bg-red-600 hover:bg-red-600 focus:ring-amber-500 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-red-600 dark:hover:bg-red-600 dark:focus:ring-amber-500">Quiz</a>
+<a href="/quiz"  type="button" class="mt-40 ml-10 text-black bg-red-600 hover:bg-red-600 focus:ring-amber-500 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-red-600 dark:hover:bg-red-600 dark:focus:ring-amber-500">Quiz</a>
 
 
 
