9be161efad04095d2747ecd1f85f4771325c999b
Dr. Doofus <>
2022-02-22 09:38:08 +0100

    added gitignore

--- resources/views/includes.php
+++ resources/views/includes.php
@@ -2,7 +2,6 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link href="./css/app.css" rel="stylesheet">
 <link href="./css/sass.css" rel="stylesheet">
-<link href="./css/main.css" rel="stylesheet">
 <link href="./js/app.js" rel="text/javascript">
 <script src="https://unpkg.com/flowbite@1.3.3/dist/flowbite.js"></script>
 <link rel="preconnect" href="https://fonts.googleapis.com">
