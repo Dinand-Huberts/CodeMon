555ea834cb02d1f6a137ae44973c606c2dfce460
dinandt <hubertsdinand@gmail.com>
2022-02-23 08:40:54 +0100

    edited gitignore

---
+++ resources/views/quiz.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head>
+
+@include('includes')
+
+</head>
+<body>  
+<div class="flex flex-col h-screen justify-between">
+    @include('header')
+
+div.quiz
+
+    @include('footer')
+</div>
+</body>
+</html>
