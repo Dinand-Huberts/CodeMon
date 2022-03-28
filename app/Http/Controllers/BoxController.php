850f289d4eef28f0cbc7568a35a8ef1891693415
dinandt <hubertsdinand@gmail.com>
2022-03-28 08:59:10 +0200

    hjdghwehyubvdxyurbndfuxfhjuergb

--- app/Http/Controllers/BoxController.php
+++ app/Http/Controllers/BoxController.php
@@ -14,6 +14,63 @@ class BoxController extends Controller
 {
     public function index($layout = true)
     {
+                session_start();
+                //Get and set user_id
+                $user_id = Auth::id();
+
+                //Set cooldown variables
+                $cooldown_query = DB::table('users')->where('id', '=', $user_id)->get();
+                $cooldown_string = $cooldown_query[0]->quiz_cooldown;
+                $cooldown = strtotime($cooldown_string);
+                $cooldown_until = $cooldown + 14400;
+                $time = time();
+                $interval = $time - $cooldown;
+                //Cooldown check
+                if ($interval >= 14400) {
+                    $onload = '';
+                    $quiz_cooldown_check = true;
+                } elseif ($interval < 14400) {
+                    $onload = 'countdown_quiz(' . $cooldown_until  . ')';
+                    $quiz_cooldown_check = false;
+                }
+        
+                if ($_GET) {
+                    if (isset($_SESSION['quiz_id'])) {
+                        //get and set user_id
+                        $user_id = Auth::id();
+        
+                        //Get quiz info with corresponding id
+                        $quiz = DB::table('quizzes')
+                            ->where('id', '=', $_SESSION['quiz_id'])
+                            ->get();
+                        $quiz_answer = $quiz[0]->quiz_answer;
+                        $quiz_difficulty = $quiz[0]->quiz_difficulty;
+                        $user_answer_boolean = $_GET['user_answer'] == $quiz_answer;
+                        if ($user_answer_boolean) {
+                            $users_data = ['quiz_cooldown' => date('Y-m-d, H:i:s', time())];
+                            $boxes_data = ['user_id' => $user_id, 'box_difficulty' => $quiz_difficulty];
+                            DB::table('boxes')->insert($boxes_data);
+                            DB::table('users')
+                                ->where('id', '=', $user_id)
+                                ->update($users_data);
+                            echo "<script>alert('Goed antwoord!')</script>";
+                        } else {
+                            $users_data = ['quiz_cooldown' => date('Y-m-d, H:i:s', time())];
+                            DB::table('users')
+                                ->where('id', '=', $user_id)
+                                ->update($users_data);
+                            echo "<script>alert('Fout antwoord!')</script>";
+                        }
+                        unset($_SESSION['quiz_id']);
+                    } else {
+                        //reroute user to page with no parameters
+                        $host = $_SERVER['HTTP_HOST'];
+                        $uri = 'codemon.test/';
+                        header('Location: /dashboard');
+                        exit();
+                    }
+                }
+        
         $boxes = Box::where('user_id', '=', Auth::id())
             ->where('box_activated', '=', 0)
             ->get();
