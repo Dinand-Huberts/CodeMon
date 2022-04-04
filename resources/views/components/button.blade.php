b2208ff6e54a72ab70e55a1a0df81477ade030ae
jploeg <jploeg3450@student.landstede.nl>
2022-04-04 09:47:49 +0200

    homepage fix

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,3 +1,4 @@
 <button type="submit"
-class="text-black bg-red-600 hover:bg-red-700 focus:ring-amber-500 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-red-600 mt-10">
-{{$slot}}</button>
+    class="text-black bg-red-600 hover:bg-red-700 focus:ring-amber-500 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-red-600 mt-10">
+    {{ $slot }}
+</button>
